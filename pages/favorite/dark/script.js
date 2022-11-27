b4f509dc45861404018a871ac57fa53cb157dccc
Zohre <zohreshabanpour.hacktor.com>
2022-11-27 19:18:19 +0330

    designed more pages

--- pages/favorite/dark/script.js
+++
@@ -1,8 +0,0 @@
-
-function changeCount(isIncrease) {
-    let control = document.getElementById('productCount');
-    let count = parseInt(control.value );
-    count = isIncrease ? count+=1 : (count === 1 ? 1 : count-=1);
-    control.value  = String(count);
-}
-
